b377360621ecd02e761d6a4d92838f2305400863
Riquelmy Fontin <fontinriquelmy@gmail.com>
2021-10-20 01:07:09 -0300

    E7-3 - Esboço de rejeicao.

--- src/main/kotlin/br/unipar/plano/domain/solicitacaoliberacaoprocedimento/service/SolicitacaoLiberacaoProcedimentoService.kt
+++ src/main/kotlin/br/unipar/plano/domain/solicitacaoliberacaoprocedimento/service/SolicitacaoLiberacaoProcedimentoService.kt
@@ -3,6 +3,7 @@ package br.unipar.plano.domain.solicitacaoliberacaoprocedimento.service
 import br.unipar.plano.domain.solicitacaoliberacaoprocedimento.model.SolicitacaoLiberacaoProcedimento
 import br.unipar.plano.domain.solicitacaoliberacaoprocedimento.model.StatusSolicitacaoLiberacaoProcedimento
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class SolicitacaoLiberacaoProcedimentoService {
@@ -18,7 +19,15 @@ class SolicitacaoLiberacaoProcedimentoService {
         // consultar solicitacao e atualizar para liberada
     }
 
-    fun rejeitarSolicitacao(solicitacaoLiberacaoProcedimento: SolicitacaoLiberacaoProcedimento) {
-        solicitacaoLiberacaoProcedimento.statusSolicitacao = StatusSolicitacaoLiberacaoProcedimento.REJEITADO;
+    fun rejeitarSolicitacao(solicitacaoID: Long, descricaoRejeicao: String){
+        if(Objects.nonNull(Long) && Objects.nonNull(descricaoRejeicao)){
+            // TODO consulta solicitacao
+            val solicitacao: SolicitacaoLiberacaoProcedimento? = null;
+
+            if(solicitacao!!.statusSolicitacao.equals(StatusSolicitacaoLiberacaoProcedimento.ABERTO)){
+                solicitacao.statusSolicitacao = StatusSolicitacaoLiberacaoProcedimento.REJEITADO;
+                solicitacao.descricaoRejeicao = descricaoRejeicao;
+            }
+        }
     }
 }
