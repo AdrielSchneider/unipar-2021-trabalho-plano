a1d708d7df0f747af4d3ebfb240515bc18b4ac1c
Riquelmy Fontin <fontinriquelmy@gmail.com>
2021-10-20 01:04:56 -0300

    E7-1 - comentarios e atualizacao de classe.

--- src/main/kotlin/br/unipar/plano/domain/solicitacaoliberacaoprocedimento/service/SolicitacaoLiberacaoProcedimentoService.kt
+++ src/main/kotlin/br/unipar/plano/domain/solicitacaoliberacaoprocedimento/service/SolicitacaoLiberacaoProcedimentoService.kt
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service
 @Service
 class SolicitacaoLiberacaoProcedimentoService {
 
-    fun insert(solicitacaoLiberacaoProcedimento: SolicitacaoLiberacaoProcedimento){
-        //fazer DTO para validar os dados
-
+    fun insert(solicitacaoLiberacaoProcedimento: SolicitacaoLiberacaoProcedimento) {
+//        validar carteirinha
+//        verificar procedimento restrito
+//        verificar se o medico ta diferente de null
     }
 
-    fun liberarSoliciatacao(solicitacaoLiberacaoProcedimento: SolicitacaoLiberacaoProcedimento){
-        solicitacaoLiberacaoProcedimento.statusSolicitacao = StatusSolicitacaoLiberacaoProcedimento.LIBERADO;
+    fun liberarSoliciatacao(usuario: String, procedimentoID: Long, solicitacaoID: Long) {
+        // consultar se ha alguma solicitacao para o procedimento naquele mes
+        // consultar solicitacao e atualizar para liberada
     }
 
-    fun rejeitarSolicitacao(solicitacaoLiberacaoProcedimento: SolicitacaoLiberacaoProcedimento){
+    fun rejeitarSolicitacao(solicitacaoLiberacaoProcedimento: SolicitacaoLiberacaoProcedimento) {
         solicitacaoLiberacaoProcedimento.statusSolicitacao = StatusSolicitacaoLiberacaoProcedimento.REJEITADO;
     }
 }
